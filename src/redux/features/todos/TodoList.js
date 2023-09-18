3b5243ecd5f0a12f6d80303e1000e22221c4b3bc
Masood Acheampong <masoodacheampong@gmail.com>
2023-09-18 23:56:11 +0100

    Redux-fundamentals

---
+++ src/redux/features/todos/TodoList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useSelector, shallowEqual } from "react-redux";
+import TodoListItem from "./TodoListItem";
+
+const selectTodoIds = (state) => state.todos.map((todo) => todo.id);
+
+const TodoList = () => {
+  const todoIds = useSelector(selectTodoIds, shallowEqual);
+  const selectTodos = (state) => state.todos;
+
+  const todos = useSelector(selectTodos);
+
+  const renderedListItems = todoIds.map((todoId) => {
+    return <TodoListItem key={todoId} id={todoId} />;
+  });
+  return <ul className="todo-list">{renderedListItems}</ul>;
+};
+
+export default TodoList;
